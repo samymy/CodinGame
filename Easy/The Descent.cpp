15c71883726f1dd774acbecbf20ce3a39c97fdfb
Vadym Dubinkin <39004496+samymy@users.noreply.github.com>
2018-09-10 18:09:40 +0300

    Create The Descent.cpp

---
+++ Easy/The Descent.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+int main()
+{
+    // game loop
+    while (1) {
+        
+        vector<int> mountainHs;
+        
+        for (int i = 0; i < 8; i++) {
+            int mountainH; // represents the height of one mountain.
+            cin >> mountainH; cin.ignore();
+            mountainHs.push_back(mountainH);
+        }
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+        typename vector<int>::const_iterator max;
+        max = max_element(mountainHs.cbegin(), mountainHs.cend());
+
+        cout << max - mountainHs.begin() << endl; // The index of the mountain to fire on.
+        // cout << distance(mountainHs.begin(), max) << endl;
+        
+        mountainHs.clear();
+    }
+}
